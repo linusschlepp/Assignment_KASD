80f69eb3b4bd0f6af8339b7bfc17b9037d677bd3
linus <linusmo.94@googlemail.com>
2022-05-01 15:24:19 +0200

    added login/ signup templates

--- src/web-routes.js
+++ src/web-routes.js
@@ -1,3 +1,10 @@
 import { dashboardController } from "./controllers/dashboard-controller.js";
+import { accountsController } from "./controllers/acccounts-controller.js";
 
-export const webRoutes = [{ method: "GET", path: "/", config: dashboardController.index }];
+export const webRoutes = [
+  { method: "GET", path: "/", config: dashboardController.index },
+  { method: "GET", path: "/signup", config: accountsController.showSignup },
+  { method: "GET", path: "/login", config: accountsController.showLogin },
+  { method: "POST", path: "/register", config: accountsController.signup },
+  { method: "POST", path: "/authenticate", config: accountsController.login },
+];
